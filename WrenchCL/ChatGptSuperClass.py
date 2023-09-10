6fadd43739bad7aa3ed84aea83e4ace753393907
Kydoimos97 <willemvanderschans97@gmail.com>
2023-09-10 16:54:39 -0600

    if self.request_url is None or self.chat_gpt_key is None: added arguments for request url and key to omit initialization and namespace issues.

--- WrenchCL/ChatGptSuperClass.py
+++ WrenchCL/ChatGptSuperClass.py
@@ -14,55 +14,19 @@ from WrenchCL.WrenchLogger import wrench_logger
 
 class ChatGptSuperClass:
 
-    def __init__(self, secrets_path='../resources/secrets/Secrets.env'):
+    def __init__(self, endpoint=None, key=None):
         self.returned_response = None
         self.response = None
-        self.secrets_input = pathlib.Path(secrets_path)
-        self.secrets_path = pathlib.Path(self.secrets_input)
         self.message = None
+        self.request_url = endpoint
+        self.chat_gpt_key = key
 
-    def load_configuration(self):
-        # Add to readme how to overwrite secrets, so we can handle them where needed
-        self.secrets_path = os.path.abspath(os.path.join(os.getcwd(), self.secrets_input))
-        if 'CHATGPT_API_KEY' in globals() and 'CHATGPT_API_ENDPOINT' in globals():
-            wrench_logger.debug(f'Found Secrets variables')
-            openai.api_key = CHATGPT_API_KEY
-            self.request_url = CHATGPT_API_ENDPOINT
-        elif os.getenv('CHATGPT_API_KEY') is not None and os.getenv('CHATGPT_API_ENDPOINT') is not None:
-            wrench_logger.debug(f'Found Environment variables')
-            openai.api_key = os.getenv('CHATGPT_API_KEY')
-            self.request_url = os.getenv('CHATGPT_API_ENDPOINT')
+        if self.request_url is None or self.chat_gpt_key is None:
+            wrench_logger.error(f"Please provide the API key and endpoint")
         else:
-            self._secrets_finder()
-            load_dotenv(self.secrets_path)
-            openai.api_key = os.getenv('CHATGPT_API_KEY')
-            self.request_url = os.getenv('CHATGPT_API_ENDPOINT')
-        wrench_logger.debug(f'Found request url: {self.request_url}')
-
-    def _secrets_finder(self):
-        parent_count = 0
-        while True:
-            time.sleep(0.05)
-            try:
-                if parent_count > 5:
-                    raise ValueError('Maximum parent count reached.')
-
-                if pathlib.Path(self.secrets_path).is_file():
-                    wrench_logger.info(f"Found Secrets file at {self.secrets_path}")
-                    break  # Exit loop if file exists
-                else:
-                    wrench_logger.debug(f'File not found: {self.secrets_path}. Trying parent directory...')
-                    root_folder = pathlib.Path.cwd()
-                    for _ in range(parent_count):
-                        root_folder = root_folder.parent
-                    self.secrets_path = os.path.join(root_folder, self.secrets_input)
-                    parent_count += 1
-            except ValueError as ve:
-                wrench_logger.error(f'No suitable secret path found after 5 iterations. Check your secrets path.')
-                raise ve
-            except Exception as e:
-                wrench_logger.error(f'An unexpected error occurred while loading secrets | {e}')
-                raise e
+            openai.api_key = self.chat_gpt_key
+            self.request_url = self.request_url
+        self.fetch_response()
 
     def fetch_response(self):
         if self.message is None or self.function is None:
@@ -88,8 +52,6 @@ class ChatGptSuperClass:
         # These functions are meant to be overwritten in a subclass
         raise NotImplementedError
         pass
-
-
 
     def _function_generator(self):
         # This function is meant to be overwritten in a subclass.
