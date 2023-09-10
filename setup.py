a48d188366791cdf97aa2c288bc164ff29e391da
Willem van der Schans <63725110+Kydoimos97@users.noreply.github.com>
2023-09-10 11:52:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='wrench-code-library',
-    version='1.0.0',
+    version='AUTO_BUMP_VERSION',
     author='willem@wrench.ai',
     long_description=long_description,
     long_description_content_type="text/markdown",  # or "text/x-rst" if you used reStructuredText
