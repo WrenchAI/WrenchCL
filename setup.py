bd7b84822421ff39c6448dc25eaaa76ac8054dfd
Kydoimos97 <willemvanderschans97@gmail.com>
2023-09-19 15:26:29 -0600

    Dependency Update

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
-# Read the long description from README.md or README.rst
+# Read the long description from README.md
 with open("README.md", "r") as f:
     long_description = f.read()
+
+# Read the dependencies from requirements.txt
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
 
 setup(
     name='WrenchCL',
     version='AUTO_BUMP_VERSION',
     author='willem@wrench.ai',
     long_description=long_description,
-    long_description_content_type="text/markdown",  # or "text/x-rst" if you used reStructuredText
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[
-        'colorama~=0.4.6',
-        'pandas~=1.5.3',
-        'requests~=2.29.0',
-        'psycopg2~=2.9.6',
-        'python-dotenv~=1.0.0',
-        'openai~=0.27.4',
-        'tenacity~=8.2.2',
-    ],
+    install_requires=required,
+    extras_require={
+        'optional': ['colorama>=0.4.6']
+    }
 )
