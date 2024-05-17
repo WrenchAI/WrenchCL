33526fb9ba3267e1d3bfb08b811f69eb14973876
Kydoimos97 <willemvanderschans97@gmail.com>
2024-05-17 11:36:37 -0600

    Refactored Singleton pattern in WrenchLogger and adjusted imports
    The SingletonClass decorator in WrenchLogger.py was replaced with a new implementation using LoggerSingleton. Additionally, the import order in __init__.py was adjusted accordingly.
    
    This refactoring guarantees proper memory management and synchronization by ensuring a single instance of the _wrench_logger class. The adjusted imports in __init__.py enhance the code's readability and maintainability.

--- WrenchCL/Tools/WrenchLogger.py
+++ WrenchCL/Tools/WrenchLogger.py
@@ -25,8 +25,6 @@ from datetime import datetime, timedelta
 from textwrap import fill
 from typing import Any, Optional
 
-from ..Decorators.SingletonClass import SingletonClass
-
 try:
     from colorama import init, Fore as ColoramaFore, Style as ColoramaStyle
 
@@ -35,7 +33,52 @@ except ImportError:
     colorama_imported = False
     logging.warning("colorama package not found. Colored logging is disabled.")
 
-@SingletonClass
+
+def LoggerSingleton(cls):
+    """
+    Decorator for a class to implement the Singleton pattern. This decorator ensures that only one instance of the
+    decorated class can exist at any time. If an instance of the class already exists, it returns that instance;
+    otherwise, it creates and returns a new instance.
+
+    The Singleton pattern is particularly useful when exactly one object is needed to coordinate actions across the
+    system, such as in the case of managing database connections.
+
+    :param cls: The class to be decorated as a Singleton.
+    :type cls: type
+    :returns: A wrapper function that manages the instantiation of the singleton class, ensuring that only one
+              instance exists.
+    :rtype: function
+
+    **Example**::
+
+        >>> @SingletonClass
+        ... class DatabaseManager:
+        ...     def __init__(self, connection_string):
+        ...         self.connection_string = connection_string
+        ...
+        >>> db_manager1 = DatabaseManager('db_connection_string')
+        >>> db_manager2 = DatabaseManager('db_connection_string')
+        >>> assert db_manager1 is db_manager2  # Both variables point to the same instance
+    """
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        """
+        Returns the singleton instance of the class, creating it if it does not already exist.
+
+        :param args: Positional arguments for the class constructor.
+        :param kwargs: Keyword arguments for the class constructor.
+        :returns: The singleton instance of the class.
+        """
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+
+
+@LoggerSingleton
 class _wrench_logger:
 
     def __init__(self, level: str = 'INFO', file_logging=False, file_name_append_mode: Optional[str] = None) -> None:
