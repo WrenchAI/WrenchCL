926406f1792ba16961d02d4ac7bfd08b57245542
Kydoimos97 <willemvanderschans97@gmail.com>
2024-05-16 21:39:33 -0600

    Removed MIT License boilerplate from AWSClientHub.py
    The MIT License boilerplate and redundant comments were removed from AWSClientHub.py for cleaner and more efficient code. Authorship and permissions details were removed as they were repeated in the file after the license paragraph. Now, the file starts with the tool import commands directly for better readability.

--- WrenchCL/Connect/AWSClientHub.py
+++ WrenchCL/Connect/AWSClientHub.py
@@ -13,17 +13,6 @@
 #  For inquiries, please contact Willem van der Schans through the official Wrench.AI channels or directly via GitHub at [Kydoimos97](https://github.com/Kydoimos97).
 #
 
-#
-#  MIT License
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#
-#
-#  All works within the Software are owned by their respective creators and are distributed by Wrench.AI.
-#
-#  For inquiries, please contact Willem van der Schans through the official Wrench.AI channels or directly via GitHub at [Kydoimos97](https://github.com/Kydoimos97).
-#
 
 import json
 import psycopg2
