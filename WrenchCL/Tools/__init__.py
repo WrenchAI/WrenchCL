c56a884522ddf644ed9728a41b1875433e04b1f8
Kydoimos97 <willemvanderschans97@gmail.com>
2024-05-31 13:38:08 -0600

    Update imports in Tools __init__ file
    Updated the import script on Tools __init__ script. New methods namely validate_base64, get_hash from Image2B64 and typechecker from TypeChecker, along with single_quote_decoder from JsonSerializer have been added. Removed validate_input_dict from DictValidator.

--- WrenchCL/Tools/__init__.py
+++ WrenchCL/Tools/__init__.py
@@ -3,9 +3,10 @@
 from .WrenchLogger import logger
 from .Coalesce import coalesce
 from .FileTyper import get_file_type
-from .Image2B64 import image_to_base64
+from .Image2B64 import image_to_base64, validate_base64, get_hash
 from .MaybeMonad import Maybe
-from .DictValidator import validate_input_dict
+from .TypeChecker import typechecker
 from .FetchMetaData import get_metadata
-from .JsonSerializer import robust_serializer
-__all__ = ['coalesce', 'get_file_type', 'image_to_base64', 'Maybe', 'logger', 'validate_input_dict', 'get_metadata', 'robust_serializer']
+from .JsonSerializer import robust_serializer, single_quote_decoder
+
+__all__ = ['coalesce', 'get_file_type', 'image_to_base64', 'Maybe', 'logger', 'typechecker', 'get_metadata', 'robust_serializer', 'validate_base64', 'single_quote_decoder']
