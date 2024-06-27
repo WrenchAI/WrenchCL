812b8434d39a7fc2357d7e039d6858361dad69cb
Kydoimos97 <willemvanderschans97@gmail.com>
2024-06-27 11:58:00 -0600

    Refactor Retryable decorator to support async calls
    The commit refactors the Retryable decorator in the WrenchCL library to support asynchronous function calls. It separates the functionality into synchronous and asynchronous wrappers depending on the function's nature. Additionally, unnecessary comments and usage examples were removed for brevity and clarity. The changes aim to enhance the

--- WrenchCL/Decorators/Retryable.py
+++ WrenchCL/Decorators/Retryable.py
@@ -1,19 +1,5 @@
-#  Copyright (c) $YEAR$. Copyright (c) $YEAR$ Wrench.AI., Willem van der Schans, Jeong Kim
-#
-#  MIT License
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#  All works within the Software are owned by their respective creators and are distributed by Wrench.AI.
-#
-#  For inquiries, please contact Willem van der Schans through the official Wrench.AI channels or directly via GitHub at [Kydoimos97](https://github.com/Kydoimos97).
-#
-
 import time
+import asyncio
 from functools import wraps
 from json import JSONDecodeError
 import requests
@@ -48,28 +34,7 @@ def Retryable(max_retries=5, retry_on_exceptions=None, delay=2, verbosity=1, log
     :type logging_level: str
 
     :return: The result of the decorated function, if it succeeds within the allowed retries.
-
-    Usage example:
-        >>> import json
-        >>> import requests
-        >>> from botocore.exceptions import ClientError, BotoCoreError
-        >>> from WrenchCL import Retryable
-        >>> from json import JSONDecodeError
-
-        >>> @Retryable(max_retries=3, retry_on_exceptions=(ValueError,), verbosity=3, logging_level="WARNING")
-        >>> def fetch_data(url):
-        >>>     response = requests.get(url)
-        >>>     response.raise_for_status()
-        >>>     return response.json()
-        >>> try:
-        >>>     result = fetch_data("https://api.example.com/data")
-        >>> except (JSONDecodeError, requests.exceptions.RequestException):
-        >>>     print("Failed to fetch data after multiple retries.")
-
-    This will retry the `fetch_data` function up to 3 times if it encounters a `JSONDecodeError`, specific `requests` exceptions,
-    `botocore` exceptions, or any other specified exceptions. The logging level is set to "DEBUG", and warnings and errors are logged.
     """
-
     if retry_on_exceptions is None:
         retry_on_exceptions = (Exception,)
 
@@ -81,7 +46,73 @@ def Retryable(max_retries=5, retry_on_exceptions=None, delay=2, verbosity=1, log
 
     def decorator_retry(func):
         @wraps(func)
-        def wrapper_retry(*args, **kwargs):
+        async def async_wrapper(*args, **kwargs):
+            retry_count = 0
+            logger.setLevel(logging_level.upper())
+
+            while retry_count < max_retries:
+                try:
+                    response = await func(*args, **kwargs)
+                    if hasattr(response, 'status_code') and response.status_code != 200:
+                        response.raise_for_status()
+                    logger.revertLoggingLevel()
+                    return response
+                except (JSONDecodeError, requests.exceptions.HTTPError, requests.exceptions.ConnectionError,
+                        requests.exceptions.Timeout, requests.exceptions.RequestException, ClientError,
+                        BotoCoreError) as e:
+                    error_message = f"Retryable error occurred: {e}"
+                    if retry_count + 1 < max_retries:
+                        if verbosity >= 3:
+                            logger.warning(
+                                f"{retry_count + 1}/{max_retries} Retrying request. \n Error: {error_message}")
+                        if verbosity == 4:
+                            log_state(func, *args, **kwargs)
+                        retry_count += 1
+                        await asyncio.sleep(delay)
+                    else:
+                        if verbosity >= 1:
+                            logger.error(f"Failed {max_retries} retries, \n Error: {error_message}")
+                        if verbosity >= 2:
+                            log_state(func, *args, **kwargs)
+                        logger.revertLoggingLevel()
+                        raise e
+                except retry_on_exceptions as e:
+                    error_message = f"Specified retryable error occurred: {e}"
+                    if retry_count + 1 < max_retries:
+                        if verbosity >= 3:
+                            logger.warning(
+                                f"{retry_count + 1}/{max_retries} Retrying request. \n Error: {error_message}")
+                        if verbosity == 4:
+                            log_state(func, *args, **kwargs)
+                        retry_count += 1
+                        await asyncio.sleep(delay)
+                    else:
+                        if verbosity >= 1:
+                            logger.error(f"Failed {max_retries} retries, \n Error: {error_message}")
+                        if verbosity >= 2:
+                            log_state(func, *args, **kwargs)
+                        logger.revertLoggingLevel()
+                        raise e
+                except Exception as e:
+                    error_message = f"Unhandled error occurred: {e}"
+                    if retry_count + 1 < max_retries:
+                        if verbosity >= 3:
+                            logger.warning(f"{retry_count + 1}/{max_retries} Retrying request: failed with error {e}")
+                        if verbosity == 4:
+                            log_state(func, *args, **kwargs)
+                        retry_count += 1
+                        await asyncio.sleep(delay)
+                    else:
+                        if verbosity >= 1:
+                            logger.error(f"Failed {max_retries} retries, \n Error: {error_message}")
+                        if verbosity >= 2:
+                            log_state(func, *args, **kwargs)
+                        logger.revertLoggingLevel()
+                        raise e
+            logger.revertLoggingLevel()
+
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
             retry_count = 0
             logger.setLevel(logging_level.upper())
 
@@ -146,6 +177,9 @@ def Retryable(max_retries=5, retry_on_exceptions=None, delay=2, verbosity=1, log
                         raise e
             logger.revertLoggingLevel()
 
-        return wrapper_retry
+        if asyncio.iscoroutinefunction(func):
+            return async_wrapper
+        else:
+            return sync_wrapper
 
     return decorator_retry
