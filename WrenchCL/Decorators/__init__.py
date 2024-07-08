79e6c3225f3e0fefeb4bcfe57748a9c82ac2b17b
Kydoimos97 <willemvanderschans97@gmail.com>
2024-07-08 10:15:02 -0600

    Remove MultiThreaded decorator
    The MultiThreaded.py file and its references were removed as they are no longer needed within the WrenchCL/Decorators directory. This commit also updates the __init__.py file to reflect this change.

--- WrenchCL/Decorators/__init__.py
+++ WrenchCL/Decorators/__init__.py
@@ -3,6 +3,5 @@
 from .Retryable import Retryable
 from .SingletonClass import SingletonClass
 from .TimedMethod import TimedMethod
-from .MultiThreaded import MultiThreaded
 
-__all__ = ['Retryable', 'SingletonClass', 'TimedMethod', 'MultiThreaded']
+__all__ = ['Retryable', 'SingletonClass', 'TimedMethod']
