4519444e6b036541f451e9789c9d1c3c82e7340e
Kydoimos97 <willemvanderschans97@gmail.com>
2024-08-12 16:58:49 -0600

    Deprecate pre-instantiated logger; add Synchronized decorator
    Deprecate the pre-instantiated 'logger' object in favor of using 'Logger' directly, with a warning issued for backward compatibility. Also added the 'Synchronized' decorator to the list of exports in WrenchCL/Decorators.

--- WrenchCL/__init__.py
+++ WrenchCL/__init__.py
@@ -1,6 +1,20 @@
-# WrenchCL/__init__.py
-
+import warnings
 from .Tools.WrenchLogger import Logger
+
+# Check if 'logger' has been imported from 'WrenchCL.Tools.WrenchLogger'
+try:
+    from .Tools.WrenchLogger import logger
+    # If 'logger' is imported, issue a warning and reassign
+    warnings.warn(
+        "The pre-instantiated 'logger' object is deprecated and will be removed in a future release. Please use from' WrenchCL import Logger' instead and instantiate.",
+        DeprecationWarning,
+        stacklevel=2
+    )
+except ImportError:
+    # 'logger' is not imported; nothing to do here
+    pass
+
+# Create a new instance of Logger and assign it to `logger`
 logger = Logger()
 
 __all__ = [
