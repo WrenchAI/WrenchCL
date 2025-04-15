bf3f85b34671e969aee59f9a02cb9568d790feea
Kydoimos97 <willemvanderschans97@gmail.com>
2025-04-15 00:42:18 -0600

    Refactor logger initialization and update packaging config
    Modified logger initialization to avoid instantiation on import, ensuring consistency. Updated setuptools configuration to use package discovery, improving maintainability and flexibility for future extensions.

--- WrenchCL/__init__.py
+++ WrenchCL/__init__.py
@@ -1,6 +1,6 @@
 from .Tools.WrenchLogger import Logger
 
 # Assign the deprecated_logger to the name 'logger' for backward compatibility
-logger = Logger()
+logger = Logger
 
 __all__ = ['logger', 'Logger']
