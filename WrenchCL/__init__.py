ee4b3b2db0d545359395e229a103d8f34ded1b06
Kydoimos97 <willemvanderschans97@gmail.com>
2025-08-13 17:23:22 -0600

    Fixed relative import for `cLogger` in `WrenchCL.__init__.py` to ensure proper module resolution.

--- WrenchCL/__init__.py
+++ WrenchCL/__init__.py
@@ -1,5 +1,5 @@
 """WrenchCL - Core functionality always available."""
-from cLogger import cLogger
+from .cLogger import cLogger
 
 # noinspection PyUnusedFunction,PySameParameterValue
 logger: cLogger = cLogger()
