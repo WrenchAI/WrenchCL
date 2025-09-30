9b220a1be77c955df8c5cdf2f153948397f41355
Kydoimos97 <willemvanderschans97@gmail.com>
2025-09-30 14:22:18 -0600

    Replaced `logger.debug` with `logger._internal_log` in `JsonParser`, `TypeChecker` for consistency with updated logging practices; enhanced `robust_serializer` with additional serialization support (e.g., `Enum`, `Path`, `UUID`, `Pydantic`); added `RobustJSONEncoder` for seamless integration; introduced `truncate_display` utility for safe, truncated string generation; updated `__init__.py` to export new utilities.

--- WrenchCL/Tools/__init__.py
+++ WrenchCL/Tools/__init__.py
@@ -6,7 +6,7 @@ from .FetchMetaData import get_metadata
 from .FileTyper import get_file_type
 from .Image2B64 import image_to_base64, validate_base64, get_hash
 from .JsonParser import parse_json, safe_json_loader, list_loader, show_json_tree
-from .JsonSerializer import robust_serializer, single_quote_decoder
+from .JsonSerializer import robust_serializer, single_quote_decoder, RobustJSONEncoder
 from .MaybeMonad import Maybe
 from .StandardizeNone import standardize_none
 from .TypeChecker import typechecker
@@ -18,5 +18,5 @@ __all__ = [
     'typechecker', 'get_metadata', 'robust_serializer',
     'validate_base64', 'single_quote_decoder', 'parse_json',
     'safe_json_loader', 'list_loader', 'show_json_tree',
-        'get_hash', 'standardize_none'
+        'get_hash', 'standardize_none', RobustJSONEncoder
 ]
